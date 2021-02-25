20c3f86564a226e2674a2e30730b3ef3765455b4
kefeng <kefeng1026@gmail.com>
2021-02-25 22:20:59 +0800

    feat: init data

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+/*
+ * @Date         : 2021-02-25 22:06:06
+ * @Author       : kefeng
+ * @LastEditors  : kefeng
+ * @LastEditTime : 2021-02-25 22:13:06
+ * @FilePath     : /douban250/src/main.js
+ */
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
+<!--
+ * @Date         : 2021-02-25 22:06:06
+ * @Author       : kefeng
+ * @LastEditors  : kefeng
+ * @LastEditTime : 2021-02-25 22:19:16
+ * @FilePath     : /douban250/src/views/Home.vue
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <el-table :data="movieList">
+      <el-table-column prop="rank" label="排名" width="180"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
+import list from '@/db/list';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      movieList: list,
+    };
   },
 };
 </script>
